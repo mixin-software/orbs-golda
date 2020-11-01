9c4efa5ad12e9bad81cccff159f049effe46ab50
denis6421 <denisn@mixin.co.il>
2020-11-01 11:04:39 +0200

    Updates

---
+++ precache-manifest.8af2753e3cb52278b496af7f87a01eaf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fc69f255cfc15a6fc725da778b87f0",
+    "url": "/orbs-golda/index.html"
+  },
+  {
+    "revision": "326c8e62a07791483cb4",
+    "url": "/orbs-golda/static/css/main.96e47904.chunk.css"
+  },
+  {
+    "revision": "46fb84e8e422b8efca45",
+    "url": "/orbs-golda/static/js/2.eb936deb.chunk.js"
+  },
+  {
+    "revision": "4d9935c1add9a7f21efe13e8909f4316",
+    "url": "/orbs-golda/static/js/2.eb936deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326c8e62a07791483cb4",
+    "url": "/orbs-golda/static/js/main.04aa0ce8.chunk.js"
+  },
+  {
+    "revision": "1a3040547d5f170d2fc9",
+    "url": "/orbs-golda/static/js/runtime-main.d38e3d6f.js"
+  },
+  {
+    "revision": "5ae6ccfeb22ce53c5aaaf6e4ffe00379",
+    "url": "/orbs-golda/static/media/copy.5ae6ccfe.svg"
+  },
+  {
+    "revision": "3ee6411fe1d1c0d8e013b07249a8af90",
+    "url": "/orbs-golda/static/media/cpu.3ee6411f.svg"
+  },
+  {
+    "revision": "95190aa2983a1102d306e06ec1bdf595",
+    "url": "/orbs-golda/static/media/deligators-icon.95190aa2.svg"
+  },
+  {
+    "revision": "fc6fbf165a4e75d50d06943c982bc83b",
+    "url": "/orbs-golda/static/media/encrypted.fc6fbf16.svg"
+  },
+  {
+    "revision": "57aad41c36a29cb5ce924741031f6e57",
+    "url": "/orbs-golda/static/media/guardians-icon.57aad41c.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/orbs-golda/static/media/loupe.ecef733a.svg"
+  },
+  {
+    "revision": "863932b67840a784f8e99543ac82d9d9",
+    "url": "/orbs-golda/static/media/navbar-logo.863932b6.svg"
+  },
+  {
+    "revision": "091557cd043a610188ed9b9df9957f3b",
+    "url": "/orbs-golda/static/media/overview-icon.091557cd.svg"
+  },
+  {
+    "revision": "dd5bc28e60d6f3fa4aab7a05f5676458",
+    "url": "/orbs-golda/static/media/token.dd5bc28e.svg"
+  }
+]);
